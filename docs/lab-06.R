9056a00b05e6e45ddaf3fedde401dc94373f5c70
anguswg-ucsb <anguswatters@gmail.com>
2020-09-14 08:38:18 -0700

    lab 6 commit_4

--- docs/lab-06.R
+++ docs/lab-06.R
@@ -73,7 +73,8 @@ plot(basin_streams, add = TRUE, col = 'cyan4', lwd = 3)
 # Stream raster
 streams_buff = basin_streams %>% st_transform(5070) %>%
   st_buffer(10) %>%
-  st_transform(4326)
+  st_transform(4326) %>%
+  st_transform(st_crs(elev2))
 
 streams_rast = fasterize(streams_buff, elev2)
 writeRaster(streams_rast, 'data/mission-creek-area-streams-rast.tif', overwrite = TRUE)
@@ -129,32 +130,51 @@ hillshade_sb = hillshade %>% crop(sb)
 
 hand_sb = hand_r %>%  crop(sb)
 
-st = stack(hillshade, hand_r) %>% crop(sb)
 
-plot(st)
-?stack
+crs(sb)
+crs(builds)
+crs(basin)
+crs(basin_buildings)
+crs(builds)
 
+
+basin_sb = st_intersection(basin, sb)
+builds = st_intersection(basin_buildings, basin_sb)
 save_gif({
   for(i in 0:20) {
-    plot(hillshade, box = FALSE, axes = FALSE, col = gray.colors(256, alpha = .5), legend = FALSE, main = 'Hillshade')
-    plot(basin, add = TRUE, lwd = 2)
-    plot(hand_r, col = rev(blues9), add = TRUE, lwd = 6)
+
+    tmp = hand_sb
+    tmp[tmp >= i] = NA
+    flood_depth = extract(tmp, builds)
+    cols = ifelse(!is.na(flood_depth), 'darkred', 'black')
+
+
+    plot(hillshade_sb, box = FALSE, axes = FALSE, col = gray.colors(256, alpha = .5), legend = FALSE, main = 'Hillshade')
+    plot(basin_sb, add = TRUE, lwd = 2, col = NA)
+    plot(tmp, col = rev(blues9), add = TRUE, lwd = 6)
     plot(railway, add = TRUE, col = 'green', cex = 1, pch = 16)
-    plot(basin_buildings$geometry, add = TRUE, col = cols, pch = 16, cex = 0.08)
+    plot(builds$geometry, add = TRUE, col = cols, pch = 16, cex = 0.08)
 
   }
 }, gif_file = "data/mission-creek-fim.gif",
 width = 600, height = 600,
 delay = .7, loop = TRUE)
 
-?gifski()
 
-plot('data/mission-creek-fim.gif')
 
 writeRaster(sb, filename = 'data/santa-barbara.tif', overwrite = TRUE)
 plot(sb)
 
-basin_sb = crop(hand, sb)
+tmp = hand_sb
+tmp[tmp >= i] = NA
+flood_depth = extract(tmp, builds)
+cols = ifelse(!is.na(flood_depth), 'darkred', 'black')
+
+plot(builds$geometry, add = TRUE, col = cols, pch = 16, cex = 0.08)
+plot(hillshade_sb, box = FALSE, axes = FALSE, col = gray.colors(256, alpha = .5), legend = FALSE, main = 'Hillshade')
+plot(basin_sb, add = TRUE, lwd = 2, col = NA)
+plot(tmp, col = rev(blues9), add = TRUE, lwd = 6)
+plot(railway, add = TRUE, col = 'green', cex = 1, pch = 16)
 
 
 
